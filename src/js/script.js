a682b61ed74d8cbfa77dc29c47add36f39f96510
zet <zet9494@gmail.com>
2022-05-31 15:48:55 +1000

    57 lesson make tabs and script

--- src/js/script.js
+++ src/js/script.js
@@ -15,6 +15,7 @@
       });
   }); */ /* slick-slider jQuery */
 
+
   const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -39,3 +40,30 @@
   document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
   });  /* tiny-slider */
+
+
+
+$(document).ready(function(){      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+
+
+
+      function toggleSLide(item) {
+        $(item).each(function(i) {
+          $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
+        });
+      };
+
+      toggleSLide('.catalog-item__link');
+      toggleSLide('.catalog-item__back');
+});
+
