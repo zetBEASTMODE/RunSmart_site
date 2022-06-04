c037619061fc8b469954447cb0bdb21395335d9f
zet BEASTMODE <zet9494@gmail.com>
2022-06-04 17:51:37 +1000

    59 lesson add modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -65,5 +65,23 @@ $(document).ready(function(){
 
       toggleSLide('.catalog-item__link');
       toggleSLide('.catalog-item__back');
+
+
+      //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+    
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      })
+    }); 
+
 });
 
