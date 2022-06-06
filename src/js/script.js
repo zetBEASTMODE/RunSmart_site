9d3a81bbce96d30ca3d5d7852b72898f7aef6e0e
zet BEASTMODE <zet9494@gmail.com>
2022-06-06 20:07:59 +1000

    61 lesson add form for phone

--- src/js/script.js
+++ src/js/script.js
@@ -86,33 +86,36 @@ $(document).ready(function(){
 
     function validateForms(form) {
         $(form).validate({
-            rules: {
-              name: {
-                required: true,
-                minlength: 2
-              },
-              phone: "required",
-              email: {
-                required: true,
-                email: true
-              },
-              messages: {
-                name: {
-                    required: "Пожалуйста, введите свое имя",
-                    minlength: jQuery.validator.format("Введите {0} количество символов")
-                },
-                phone: "Пожалуйста, введите свой номер телефона",
-                email: {
-                  required: "Пожалуйста, введите свою почту",
-                  email: "Неправильно введен адрес почты"
-                }
-              }
+          rules: {
+            name: {
+              required: true,
+              minlength: 2
+            },
+            phone: "required",
+            email: {
+              required: true,
+              email: true
             }
+          },
+          messages: {
+            name: {
+              required: "Пожалуйста, введите своё имя",
+              minlength: jQuery.validator.format("Введите минимум {0} символа")
+            },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Введите адрес своей почты example@gmail.com"
+            }
+          }
         });
     }
 
-    validateForms('consultation-form');
+    validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
+
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
 
