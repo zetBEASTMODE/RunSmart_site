f1cd420875732935a0aefacf0295f0f8fddf598b
zet <zet9494@gmail.com>
2022-06-06 16:27:47 +1000

    60 lesson add validation form

--- src/js/script.js
+++ src/js/script.js
@@ -83,5 +83,36 @@ $(document).ready(function(){
       })
     }); 
 
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+              name: {
+                required: true,
+                minlength: 2
+              },
+              phone: "required",
+              email: {
+                required: true,
+                email: true
+              },
+              messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} количество символов")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+              }
+            }
+        });
+    }
+
+    validateForms('consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 });
 
