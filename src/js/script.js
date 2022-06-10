5d299e75beef448390a9f6efc1eb9b39d1be10fb
zet <zet9494@gmail.com>
2022-06-10 16:42:31 +1000

    63 lesson add php mailer

--- src/js/script.js
+++ src/js/script.js
@@ -117,5 +117,20 @@ $(document).ready(function(){
 
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function () {
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('overlay, #thanks').fadeIn('slow');
+        $('form').trigger('reset');
+      });
+      return false;
+    });
 });
 
