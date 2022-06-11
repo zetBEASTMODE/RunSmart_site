f2cb60de1d1e9f4fd3b801d16e4a662fd86f611e
zet BEASTMODE <zet9494@gmail.com>
2022-06-11 17:34:55 +1000

    65 and 66 lesson add scroll and animate

--- src/js/script.js
+++ src/js/script.js
@@ -132,5 +132,22 @@ $(document).ready(function(){
       });
       return false;
     });
+
+
+    //Smooth scroll and pageup
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    $("a[href^='#']").click(function() {
+      var _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    })
 });
 
