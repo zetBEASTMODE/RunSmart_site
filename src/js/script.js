7feb1a509854ea2d9dbd1141cc09855a029b5d65
zet BEASTMODE <zet9494@gmail.com>
2022-06-12 22:08:52 +1000

    69 lesson rework

--- src/js/script.js
+++ src/js/script.js
@@ -144,7 +144,7 @@ $(document).ready(function(){
       }
     });
 
-    $("a[href^='#']").click(function() {
+    $("a[href^='#up']").click(function() {
       var _href = $(this).attr("href");
       $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
       return false;
